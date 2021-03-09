6d9b67fd8f9241f948e0944b29160e71747e3697
Toasted201 <69630035+Toasted201@users.noreply.github.com>
2021-03-09 17:26:07 +0100

    Création listProduct()

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -28,6 +29,22 @@ class ProductController extends AbstractController
     public function showProduct(Product $product, SerializerInterface $serializer)
     {       
         $data = $serializer->serialize($product, 'json');
+
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    
+    /**
+     * @Route("/products", name="product_list", methods={"GET"})
+     */
+    public function listProduct(SerializerInterface $serializer, ProductRepository $productRepository)
+    {
+        $products = $productRepository->findAll();
+
+        $data = $serializer->serialize($products, 'json');
 
         $response = new Response($data);
         $response->headers->set('Content-Type', 'application/json');
