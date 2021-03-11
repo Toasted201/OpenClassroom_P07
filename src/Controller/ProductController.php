38bbfd666d0ff8be053fa359ee024e3f277fc021
Toasted201 <69630035+Toasted201@users.noreply.github.com>
2021-03-11 15:33:25 +0100

    ProductController Refactoring : Remplacer Response par JSONRespone

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,35 +5,18 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use JMS\Serializer\SerializerInterface;
-
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ProductController extends AbstractController
 {
-    /**
-     * @Route("/product", name="product")
-     */
-    public function index(): Response
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductController.php',
-        ]);
-    }
-
     /**
      * @Route("/products/{id}", name="product_show", methods={"GET"})
      */
     public function showProduct(Product $product, SerializerInterface $serializer)
     {       
-        $data = $serializer->serialize($product, 'json');
-
-        $response = new Response($data);
-        $response->headers->set('Content-Type', 'application/json');
-
-        return $response;
+        return new JsonResponse($serializer->serialize($product, 'json'),200,[],true);
     }
 
     
@@ -43,12 +26,6 @@ class ProductController extends AbstractController
     public function listProduct(SerializerInterface $serializer, ProductRepository $productRepository)
     {
         $products = $productRepository->findAll();
-
-        $data = $serializer->serialize($products, 'json');
-
-        $response = new Response($data);
-        $response->headers->set('Content-Type', 'application/json');
-
-        return $response;
+        return new JsonResponse($serializer->serialize($products, 'json'),200,[],true);
     }
 }
