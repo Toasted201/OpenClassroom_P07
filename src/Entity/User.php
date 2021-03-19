c6ad1d7d898b47b28c57f3765fce1f6923127903
Toasted201 <69630035+Toasted201@users.noreply.github.com>
2021-03-19 11:19:26 +0100

    User : Modification Critère unicité (= Email & client)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity("email")
+ * @UniqueEntity("email", "client")
  * @Serializer\ExclusionPolicy("ALL")
  * @Hateoas\Relation(
  *      "self",
